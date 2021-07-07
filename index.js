527de1197cb9443e0222248fda1bc9ca7a335a4f
IvanAntonov <ivanantonov@coherentsolutuions.com>
2021-07-07 12:51:37 +0300

    add route login info

--- index.js
+++ index.js
@@ -38,4 +38,13 @@ express()
       res.status(403).end();
     }
   })
+  .options('/api/login/info', cors(corswithCredentials))
+  .get('/api/login/info', cors(corswithCredentials), function (req, res) {
+    const info = req.cookies['username'];
+    if (info != undefined || info != null) {
+      res.json({ username: info });
+    } else {
+      res.status(403).end();
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
